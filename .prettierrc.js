013f5e2f790bb15d1dd3b304b9f1b85a340c53b2
aldhyyx <aldyzkobf@gmail.com>
2020-05-10 18:06:07 +0700

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  singleValue: true,
+  objectsInArrays: true,
+  arraysInArrays: true
+};
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/mainURLInProduction/' : '/',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Warga Sekitar'
+    }
+  }
+};
