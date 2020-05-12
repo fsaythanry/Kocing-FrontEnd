3bef6e05ce14ae9442f4f8f2428e88046ebedd17
aldhyyx <aldyzkobf@gmail.com>
2020-05-12 14:10:34 +0700

    set website container

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav"><router-link to="/">Home</router-link> |</div>
-    <router-view />
+    <div class="main-container">
+      <div id="nav"><router-link to="/">Home</router-link> |</div>
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background: #f2f2f2;
+}
+.main-container {
+  width: 486px;
+  height: 100vh;
+  margin: 0 auto;
+  background: white;
+}
+
+@media screen and (max-width: 467px) {
+  .main-container {
+    width: 100%;
+  }
 }
 </style>
