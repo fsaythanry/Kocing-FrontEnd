4882b7266396a5cb0b544f2f024739f9cefe5781
aldhyyx <aldyzkobf@gmail.com>
2020-05-13 00:56:51 +0700

    add pages not found ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import PageNotFound from '../views/PageNotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -8,14 +9,18 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Home
   },
+  {
+    path: '*',
+    component: PageNotFound
+  }
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="not-found">
+    <img src="../assets/not_found.svg" alt="not found" />
+    <h6 class="mb-0">Upss, Halaman tidak ditemukan.</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound'
+};
+</script>
+<style lang="scss" scoped>
+.not-found {
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  img {
+    margin-bottom: 32px;
+    width: 60%;
+  }
+}
+</style>
