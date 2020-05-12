710cf1b3a9e1a4c689034747c6ce2963b306939b
aldhyyx <aldyzkobf@gmail.com>
2020-05-13 02:07:08 +0700

    add covid navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <div id="nav-covid">
+      <router-link :to="{ path: `/covid19` }" class="mb-0"
+        >Pantau Perkembangan Covid-19 <span>disini!</span></router-link
+      >
+      <div @click.stop="hideCovid">
+        <font-awesome-icon
+          :icon="['fas', 'times-circle']"
+          size="lg"
+          style="cursor:pointer; opacity:.5;"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faTimesCircle);
+
 export default {
+  components: {
+    FontAwesomeIcon
+  },
   name: 'Home',
+  methods: {
+    hideCovid() {
+      event.target.closest('#nav-covid').classList.add('d-none');
+    }
+  }
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  font-size: 16px;
+  padding: 24px 16px;
+}
+#nav-covid {
+  background-color: #e7efea;
+  border-radius: 1rem;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+
+  a {
+    color: #4e4e4e;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  span {
+    color: #1a6134 !important;
+  }
+}
+</style>
