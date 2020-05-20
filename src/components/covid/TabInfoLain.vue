34a470b503f09f7982112c87c583e8a4763da132
aldhyyx <aldyzkobf@gmail.com>
2020-05-20 17:48:16 +0700

    perbaiki info covid

--- src/components/covid/TabInfoLain.vue
+++ src/components/covid/TabInfoLain.vue
@@ -1,61 +1,69 @@
 <template>
-    <div>
-        <div class="covid">
-            <p class="update font-weight-bold mb-2">Cara Pencegahan</p>
-        </div>
-        <div >
+  <div>
+    <h2 class="title">Cara Pencegahan</h2>
+    <h2 class="title">Sumber Informasi</h2>
 
+    <div class="c-card-lain-wrapper">
+      <a href="">
+        <div class="c-card-lain">
+          <p class="m-0">Covid-19 Indonesia</p>
+          <img src="../../assets/Icon awesome-external-link-alt.svg" />
         </div>
-        <div class="covid">
-            <p class="update font-weight-bold mb-2">Link Terkait</p>
+      </a>
+      <a href="">
+        <div class="c-card-lain">
+          <p class="m-0">Kawal Covid-19</p>
+          <img src="../../assets/Icon awesome-external-link-alt.svg" />
         </div>
-        <div class="">
-            <div class="card p-3 mb-2 border-0">
-                <a href="">
-                    <p class="link float-left m-0">Covid-19 Indonesia</p>
-                    <img class="float-right" src="../../assets/Icon awesome-external-link-alt.svg" alt="">
-                </a>
-            </div>
-            <div class="card p-3 mb-2 border-0">
-                <a href="">
-                    <p class="link float-left m-0">Kawal Covid-19</p>
-                    <img class="float-right" src="../../assets/Icon awesome-external-link-alt.svg" alt="">
-                </a>
-            </div>
-            <div class="card p-3 mb-2 border-0">
-                <a href="">
-                    <p class="link float-left m-0">Corona Tracker</p>
-                    <img class="float-right" src="../../assets/Icon awesome-external-link-alt.svg" alt="">
-                </a>
-            </div>
-            <div class="card p-3 mb-2 border-0">
-                <a href="">
-                    <p class="link float-left m-0">WHO Internasional</p>
-                    <img class="float-right" src="../../assets/Icon awesome-external-link-alt.svg" alt="">
-                </a>
-            </div>
+      </a>
+      <a href="">
+        <div class="c-card-lain">
+          <p class="m-0">Corona Tracker</p>
+          <img src="../../assets/Icon awesome-external-link-alt.svg" />
         </div>
-
+      </a>
+      <a href="">
+        <div class="c-card-lain">
+          <p class="m-0">WHO Internasional</p>
+          <img src="../../assets/Icon awesome-external-link-alt.svg" />
+        </div>
+      </a>
     </div>
+  </div>
 </template>
 <script>
-import "bootstrap/dist/css/bootstrap.css";
-
 export default {
-    name: 'tabs-info-lain'
-}
+  name: 'tabs-info-lain'
+};
 </script>
-<style scoped>
-.update {
-    font-size: 16px;
-}
-.card {
-    background: #F1F1F1 0% 0% no-repeat padding-box;
-}
-.card a .link {
-    color:#1E1E1E;
-    font-size: 14px;
-    font-weight: bold;
-}
+<style lang="scss" scoped>
+.c-card-lain-wrapper {
+  display: flex;
+  flex-direction: column;
 
+  a {
+    text-decoration: none;
+  }
+
+  .c-card-lain {
+    display: flex;
+    justify-content: space-between;
+    background: #f1f1f1 0% 0% no-repeat padding-box;
+    border-radius: 8px;
+    padding: 16px;
+    margin-bottom: 8px;
+    transition: 0.25s ease;
+
+    &:focus,
+    &:active,
+    &:hover {
+      background: #d6d6d6 0% 0% no-repeat padding-box;
+    }
+
+    p {
+      font: 500 14px/17px Roboto;
+      color: #1e1e1e;
+    }
+  }
+}
 </style>
--- src/views/Covid.vue
+++ src/views/Covid.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container-covid">
-    <div>
-      <h1 class="page-title">Informasi Covid19</h1>
-    </div>
+    <h1 class="page-title">Informasi Covid19</h1>
     <tabs
       :tabs="tabs"
       :currentTab="currentTab"
@@ -14,13 +12,83 @@
     />
     <div class="content">
       <div v-if="currentTab === 'tab-indonesia'">
-        <tab-indonesia />
+        <p class="sub-title">Seluruh Indonesia</p>
+        <div class="card-covid-wrapper">
+          <div class="card-covid">
+            <p>{{ numberFormat(indonesia.cases) }}</p>
+            <p>Terkonfirmasi</p>
+          </div>
+
+          <div class="card-covid">
+            <p>{{ numberFormat(indonesia.recovered) }}</p>
+            <p>Sembuh</p>
+          </div>
+
+          <div class="card-covid">
+            <p>{{ numberFormat(indonesia.deaths) }}</p>
+            <p>Meninggal</p>
+          </div>
+
+          <div class="card-covid">
+            <p>{{ numberFormat(indonesia.active) }}</p>
+            <p>Dalam Perawatan</p>
+          </div>
+        </div>
       </div>
+
       <div v-if="currentTab === 'tab-dunia'">
-        <tab-dunia />
+        <p class="sub-title">Lihat Kasus Berdasarkan:</p>
+        <v-select
+          :options="selectOptions"
+          @input="changeByCountry"
+          :value="selectedOption"
+        >
+          <template v-slot:option="option">
+            <img
+              :src="option.icon"
+              style="width: 25px; height:25px; border-radius:25px;  margin-right: 8px; margin-bottom: 4px; margin-top:4px"
+            />
+            {{ option.label }}
+          </template>
+        </v-select>
+        <div class="card-covid-wrapper">
+          <div class="card-covid">
+            <p>
+              {{ numberFormat(country.cases) }}
+              <sup>+{{ numberFormat(country.todayCases) }}</sup>
+            </p>
+            <p>Terkonfirmasi</p>
+          </div>
+
+          <div class="card-covid">
+            <p>{{ numberFormat(country.recovered) }}</p>
+            <p>Sembuh</p>
+          </div>
+
+          <div class="card-covid">
+            <p>
+              {{ numberFormat(country.deaths) }}
+              <sup>+{{ numberFormat(country.todayDeaths) }}</sup>
+            </p>
+            <p>Meninggal</p>
+          </div>
+
+          <div class="card-covid">
+            <p>{{ numberFormat(country.active) }}</p>
+            <p>Dalam Perawatan</p>
+          </div>
+        </div>
+
+        <p class="mt-2" style="font-size: 12px;">
+          Diperbaharui pada {{ new Date(country.updated).toLocaleString() }}
+        </p>
+
+        <h2 class="title">Peta Lokasi</h2>
+        <div class="maps"></div>
       </div>
+
       <div v-if="currentTab === 'tab-lain'">
-        <tab-info-lain />
+        <tab-info-lain></tab-info-lain>
       </div>
     </div>
   </div>
@@ -28,9 +96,9 @@
 
 <script>
 import Tabs from 'vue-tabs-with-active-line';
-import TabIndonesia from '../components/covid/TabIndonesia.vue';
-import TabDunia from '../components/covid/TabDunia.vue';
-import TabInfoLain from '../components/covid/TabInfoLain.vue';
+import axios from 'axios';
+import vSelect from 'vue-select';
+import TabInfoLain from '../components/covid/TabInfoLain';
 
 const TABS = [
   { title: 'Indonesia', value: 'tab-indonesia' },
@@ -38,34 +106,227 @@ const TABS = [
   { title: 'Info Lain', value: 'tab-lain' }
 ];
 
+const APICovid = 'https://corona.lmao.ninja/v2/';
+
 export default {
   name: 'Covid19',
   components: {
     Tabs,
-    TabIndonesia,
-    TabDunia,
+    vSelect,
     TabInfoLain
   },
   data: () => ({
     tabs: TABS,
-    currentTab: 'tab-indonesia'
+    currentTab: 'tab-indonesia',
+    countries: '',
+    indonesia: '',
+    country: '',
+    selectOptions: '',
+    selectedOption: ''
   }),
+  created() {
+    this.setCountries();
+  },
+  watch: {
+    countries() {
+      this.indonesia = this.countries.find(
+        country => country.countryInfo.iso2 === 'ID'
+      );
+
+      this.selectOptions = this.countries
+        .filter(
+          country =>
+            (country.country !== null) & (country.countryInfo._id !== null)
+        )
+        .map(country => ({
+          label: country.country,
+          code: country.countryInfo._id,
+          icon: country.countryInfo.flag
+        }));
+    },
+    selectOptions() {
+      this.selectedOption = this.selectOptions.find(
+        country => country.code === 4
+      );
+    },
+    selectedOption() {
+      this.country = this.countries.find(
+        country => country.countryInfo._id === this.selectedOption.code
+      );
+    }
+  },
   methods: {
+    numberFormat(value) {
+      return !value ? '0' : Number(value.toFixed(1)).toLocaleString();
+    },
     handleClick(newTab) {
       this.currentTab = newTab;
+    },
+    setCountries() {
+      axios
+        .get(APICovid + 'countries/')
+        .then(res => {
+          if (res.status == '200') this.countries = res.data;
+          if (res.status != '200') throw new Error('Bad Server');
+        })
+        .catch(error => console.log(error));
+    },
+    changeByCountry(value) {
+      let res = this.countries
+        .filter(country => country.countryInfo._id == value.code)
+        .map(country => {
+          return {
+            label: country.country,
+            code: country.countryInfo._id,
+            icon: country.countryInfo.flag
+          };
+        });
+
+      this.selectedOption = res[0];
     }
   }
 };
 </script>
 
 <style lang="scss">
+@import 'vue-select/src/scss/vue-select.scss';
+
+.vs__dropdown-toggle {
+  height: 56px;
+  font-size: 16px;
+  padding-left: 8px;
+  background-color: #f1f1f1;
+  border: none;
+  margin-bottom: 24px;
+  border-radius: 8px;
+}
+.vs__actions {
+  padding-right: 16px;
+}
+.vs__dropdown-menu {
+  border: none;
+}
+.vs--open {
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.15);
+  & > .vs__dropdown-toggle {
+    background-color: #fff;
+    font-size: 12px;
+  }
+}
+
 .container-covid {
   padding: 24px 16px;
-  overflow: auto;
+  height: 100%;
+  overflow: hidden;
+  font-family: Roboto, 'sans-serif';
+  margin-bottom: 100px;
   .page-title {
-    font-size: 24px;
+    font-size: 20px;
     font-weight: bolder;
     margin-bottom: 40px;
+    margin-top: 0;
+    text-transform: uppercase;
+  }
+  .title {
+    margin-bottom: 16px;
+    margin-top: 32px;
+    font: Bold 16px/19px Roboto;
+  }
+  .maps {
+    width: 100%;
+    height: 400px;
+    background-color: grey;
+    display: block;
+  }
+  .sub-title {
+    font-size: 14px;
+    color: #1e1e1e;
+    margin: 0;
+    padding: 0;
+    padding-bottom: 10px;
+    font-weight: 500;
+  }
+  .card-covid-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+
+    .card-covid {
+      border-radius: 8px;
+      width: calc(50% - 8px);
+      text-align: center;
+      height: 103px;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      position: relative;
+      overflow: hidden;
+
+      &:after {
+        content: '';
+        display: block;
+        width: 205px;
+        height: 205px;
+        position: absolute;
+        top: -73px;
+        left: -73px;
+        background-size: cover;
+        opacity: 0.1;
+      }
+
+      p:nth-child(1) {
+        font: Bold 18px/22px Roboto;
+        margin: 0;
+        padding: 0;
+      }
+      p:nth-child(2) {
+        font: 12px/14px Roboto;
+        margin: 0;
+        padding: 0;
+        padding-top: 4px;
+      }
+    }
+    .card-covid:nth-child(odd) {
+      margin-right: 8px;
+    }
+    .card-covid:nth-child(even) {
+      margin-left: 8px;
+    }
+    .card-covid:nth-child(1) {
+      background: #f2ef87 0% 0% no-repeat padding-box;
+      color: #675613;
+
+      &:after {
+        background: url('../assets/1F614_black.png');
+      }
+    }
+    .card-covid:nth-child(2) {
+      background: #87f292 0% 0% no-repeat padding-box;
+      color: #0e6146;
+
+      &:after {
+        background: url('../assets/1F60A_black.png');
+      }
+    }
+    .card-covid:nth-child(3) {
+      background: #f28787 0% 0% no-repeat padding-box;
+      color: #5a0d0d;
+      margin-top: 16px;
+
+      &:after {
+        background: url('../assets/1F622_black.png');
+      }
+    }
+    .card-covid:nth-child(4) {
+      background: #87cef2 0% 0% no-repeat padding-box;
+      color: #0c4857;
+      margin-top: 16px;
+
+      &:after {
+        background: url('../assets/1F637_black.png');
+      }
+    }
   }
 }
 .default-tabs {
@@ -78,14 +339,15 @@ export default {
     padding-bottom: 8px;
     font-size: 16px;
     font-weight: bold;
-    letter-spacing: 0.8px;
-    color: gray;
+    letter-spacing: 0px;
+    color: #4e4e4e;
     text-decoration: none;
     border: none;
     background-color: transparent;
     border-bottom: 2px solid transparent;
     cursor: pointer;
     transition: all 0.25s;
+    font-family: Roboto;
     &_active {
       color: #1a6134;
     }
@@ -115,7 +377,6 @@ export default {
   }
 }
 .content {
-  margin-top: 30px;
-  font-size: 20px;
+  margin-top: 32px;
 }
 </style>
